50f73e788d8e914ad0ac2fcf9ca53d368063e136
Sam Chapman <sam.chapman@outlook.com>
2019-07-24 13:08:14 +0100

    Updated navbar to make it responsive to smaller screens

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -7,32 +7,56 @@ const CustomNavbar = ({ pageInfo }) => {
   console.log(pageInfo)
   
   return (
-    <>
-    <div className="justify-content container">     
-        <div className="d-flex flex-column justify-content container p-0" style={styles.container}> 
-          <Link to="/" className="link-no-style">
-            <div className="jumbotron jumbotron-fluid img-fluid rounded-top" style={styles.jumbo}> 
-              <div className="container">
-                <h1 className="display-1 strong" style={styles.title} >Sam Chapman .Dev</h1>
-              </div>
-            </div>
-          </Link>
+    <>    
+    {/* The below jumbotron will show on screens medium and larger (>768). This is what "d-none d-md-block" does*/}        
+        <div className="justify-content container p-0" style={styles.container}>  
 
-          <nav className="navbar navbar-dark navbar-expand-md justify-content-center rounded-bottom" style={styles.navbar}>
-            <div className="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
-              <ul className="navbar-nav mx-auto text-center">
-                <li className="nav-item nav-link"><Link to="/" className="link-no-style" style={styles.title}>Home</Link></li>
-                <li className="nav-item nav-link"><Link to="/tutorials" className="link-no-style" style={styles.title}>Tutorials</Link></li>
-                <li className="nav-item nav-link"><Link to="/projects" className="link-no-style" style={styles.title}>Projects</Link></li>
-                <li className="nav-item nav-link"><Link to="/contact" className="link-no-style" style={styles.title}>Contact</Link></li>
-                <li className="nav-item nav-link"><a href="https://github.com/kingchappers" target="_blank" rel="noopener noreferrer" className="link-no-style" style={styles.title}>GitHub</a></li>
-              </ul>
-            </div>
-          </nav>
-          
-        </div>     
-      </div>
-      
+          <div className="d-none d-md-block">
+            <Link to="/" className="link-no-style">
+              <div className="jumbotron jumbotron-fluid img-fluid rounded-top" style={styles.jumbo}> 
+                <div className="container">
+                  <h1 className="display-1 strong" style={styles.title}>Sam Chapman .Dev</h1>
+                </div>
+              </div>
+            </Link>
+
+            <nav className="navbar navbar-dark navbar-expand rounded-bottom" style={styles.navbar}>
+              <div className="navbar-collapse collapse align-items-center w-100">
+                <ul className="navbar-nav mx-auto text-center justify-content-center">
+                  <li className="nav-item nav-link col-5"><Link to="/" className="link-no-style" style={styles.title}>Home</Link></li>
+                  <li className="nav-item nav-link col-5"><Link to="/tutorials" className="link-no-style" style={styles.title}>Tutorials</Link></li>
+                  <li className="nav-item nav-link col-5"><Link to="/projects" className="link-no-style" style={styles.title}>Projects</Link></li>
+                  <li className="nav-item nav-link col-5"><Link to="/contact" className="link-no-style" style={styles.title}>Contact</Link></li>
+                  <li className="nav-item nav-link col-5"><a href="https://github.com/kingchappers" target="_blank" rel="noopener noreferrer" className="link-no-stylec" style={styles.title}>GitHub</a></li>
+                </ul>
+              </div>            
+            </nav>
+          </div>
+
+          <div className="d-md-none">
+            <Link to="/" className="link-no-style">
+              <div className="jumbotron jumbotron-fluid img-fluid rounded-top" style={styles.jumboSmall}> 
+                <div className="container">
+                  <h1 className="display-5 strong" style={styles.title}>Sam Chapman .Dev</h1>
+                </div>
+              </div>
+            </Link>
+            
+            
+            <nav className="navbar navbar-dark navbar-expand rounded-bottom" style={styles.navbar}>
+              <div className="navbar-collapse collapse align-items-center w-100">
+                <ul className="navbar-nav mx-auto text-center justify-content-center">
+                  <li className="nav-item nav-link col"><Link to="/" className="link-no-style" style={styles.title}>Home</Link></li>
+                  <li className="nav-item nav-link col"><Link to="/tutorials" className="link-no-style" style={styles.title}>Tutorials</Link></li>
+                  <li className="nav-item nav-link col"><Link to="/projects" className="link-no-style" style={styles.title}>Projects</Link></li>
+                  <li className="nav-item nav-link col"><Link to="/contact" className="link-no-style" style={styles.title}>Contact</Link></li>
+                  <li className="nav-item nav-link col"><a href="https://github.com/kingchappers" target="_blank" rel="noopener noreferrer" className="link-no-stylec" style={styles.title}>GitHub</a></li>
+                </ul>
+              </div>            
+            </nav>
+          </div>
+
+         </div>     
     </>
   )
 }
@@ -44,21 +68,26 @@ const styles ={
     width: 1500,    
   },
 
+  jumboSmall: {
+    backgroundImage: `url(${Image})`,
+    height: 150,
+    width: 600,    
+  },
+
   container: {
     backgroundColor: "#ffffff",
     position: "relative",
     minHeight: "100%",
-    width: 1500,
+    // width: 1500,
   },
 
   title: {
     color: "#f2f2f2",
-    marginRight: "5rem"
   },
 
   navbar: {
     backgroundColor: "#531A53",
-  }
+  },
 };
 
 export default CustomNavbar
